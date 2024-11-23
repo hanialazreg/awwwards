5f7716a9dfe36a7af1162199e292f4839a6027a8
hanialazreg <lazreghania@gmail.com>
2024-11-23 12:43:19 +0100

    set tailwind for vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 
 const App = () => {
-  return <div>first </div>;
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 text-bold">welcome to awwards</h1>
+    </main>
+  );
 };
 
 export default App;
