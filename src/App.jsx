4e4827344bf302196301d74ac978be4b071eb9e2
hanialazreg <lazreghania@gmail.com>
2024-11-24 22:51:52 +0100

    about section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
       <Hero />
+      <About />
     </main>
   );
 };
